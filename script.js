d943027d875d8535105cd9c2090aee9b178d457d
Sandropikademel <sandro.lima@escola.pr.gov.br>
2025-06-13 09:41:37 -0300

    GG

--- script.js
+++ script.js
@@ -25,3 +25,21 @@ botaoResetar.addEventListener('click', () => {
   contador = 0;
   atualizarContador();
 });
+contadorElemento.classList.add('flash');
+setTimeout(() => contadorElemento.classList.remove('flash'), 150);
+function gerarCorAleatoria() {
+  const r = Math.floor(Math.random() * 200) + 30; // valores não muito claros
+  const g = Math.floor(Math.random() * 200) + 30;
+  const b = Math.floor(Math.random() * 200) + 30;
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function atualizarContador() {
+  contadorElemento.textContent = contador;
+  contadorElemento.style.transform = 'scale(1.2)';
+  contadorElemento.style.color = gerarCorAleatoria(); // muda cor aqui
+  setTimeout(() => {
+    contadorElemento.style.transform = 'scale(1)';
+  }, 150);
+  localStorage.setItem('contador', contador);
+}
